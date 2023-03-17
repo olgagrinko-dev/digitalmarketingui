ca560c48618eb79fa7555f3f8eb4b73beac296f0
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-03-17 17:52:28 +0300

    project

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 // Сверстать макет с использованием препроцессоров. 
 // Добавить динамику в выпадающий список.
 // Добавить анимацию (любую, на нативном css)
+
+const btn = document.querySelector(`button`);
+
+
+btn.addEventListener(`click`, function () {
+    
+})
+
+btn.addEventListener(`click`, function (event) {    
+    p.textContent = event.target.textContent;
+})
